2ac1ffcd8fbf34d306ebe0f98bd34fd7f6fb0a45
Salman Aslam <salman.m.aslam@gmail.com>
2011-09-14 22:47:40 -0500

    clean up.

--- demo_UTIL_2D_coordinateAffineWarping_and_IntensityInterpolation.m
+++ demo_UTIL_2D_coordinateAffineWarping_and_IntensityInterpolation.m
@@ -1,3 +1,4 @@
+% I_hxw lies on a grid 1:w in x direction and 1:h in y direction
 clear;
 clc;
 close all;
@@ -12,7 +13,8 @@ close all;
     
     fp_gt                   =   [148.9306  187.2747  226.0674  169.6408  192.6433  218.8531  194.5372;
                                  179.0198  172.5994  174.0397  230.2172  224.5582  223.3328  243.8089]; %feature points (ground truth)
-  
+    [temp, G]               =   size(fp_gt);
+    
 %size of grid that i want
     w                       =   33;
     h                       =   33;
@@ -23,32 +25,39 @@ close all;
 %process grid and intensities
     [X_hxw, Y_hxw, I_hxw]   =   UTIL_2D_coordinateAffineWarping_and_IntensityInterpolation(I_0t1, Ha_2x3, w, h);  %I_0t1 means that image intensities go from 0 to 1, not required though
     
-%process feature points    
-    [fp_x, fp_y]            =   UTIL_2D_affine_apply_inverse_transform(Ha_2x3, fp_gt(1,:), fp_gt(2,:));
+%process feature points (zero center  them)    
+    [fp_x_1xG, fp_y_1xG]    =   UTIL_2D_affine_apply_inverse_transform(Ha_2x3, fp_gt(1,:), fp_gt(2,:));
+    fp_x_1xG                =   fp_x_1xG + (w-1)/2;             %image starts from (1,1), so shift by half width and half height of output image
+    fp_y_1xG                =   fp_y_1xG + (h-1)/2;             % "
 %---------------------------------------------
 %POST-PROCESSING
 %--------------------------------------------- 
 %view original figure with overlaid warped grid and feature points
     figure;
     imagesc(I_0t1);
-    colormap('gray');
-    axis equal;
-    axis tight;
-
+   
     hold on;
       
-    plot(X_hxw(:), Y_hxw(:), 'r.')      %plot grid
-    plot(fp_gt(1,:),fp_gt(2,:), 'x');   %plot feature points
-
-   
-%view warped image and feature points
-    figure;
-    imagesc(I_hxw)
+    %plot(X_hxw(:), Y_hxw(:), 'r.')      %plot grid
+    for g=1:G
+        UTIL_PLOT_filledCircle([fp_gt(1,g),fp_gt(2,g),], 5, 3000, 'b'); 
+    end
+    
     colormap('gray');
     axis equal;
     axis tight;
+
+    
+%view warped image with overlaid feature points
+    figure;
+    imagesc(I_hxw)
     
     hold on;
 
-    plot(fp_x+(w-1)/2,fp_y+(h-1)/2, 'x');
-    
+    for g=1:G
+        UTIL_PLOT_filledCircle( [fp_x_1xG(g), fp_y_1xG(g)],   1,   3000,   'b'); 
+    end
+ 
+    colormap('gray');
+    axis equal;
+    axis tight;
