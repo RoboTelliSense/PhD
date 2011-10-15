ec6dd20d4fd5a86882f4c191c29863a3af50516c
Salman Aslam <salman.m.aslam@gmail.com>
2011-10-15 17:51:51 -0500

    added double data type capability to rvq tracking.

--- RVQx_config.m
+++ RVQx_config.m
@@ -1,4 +1,4 @@
-function [aRVQx trkaRVQx]    = RVQx_config(PARAM, trkMEAN, maxQ, M, tSNR, tstI, lmbd)
+function [aRVQx trkaRVQx]    = RVQx_config(PARAM, trkMEAN, maxQ, M, tSNR, tstI, lmbd, dataType)
 
     tstD                    =   RVQ__testing_rule_string(tstI); %decode stop rule for test vectors
 
@@ -6,10 +6,10 @@ function [aRVQx trkaRVQx]    = RVQx_config(PARAM, trkMEAN, maxQ, M, tSNR, tstI, 
     aRVQx.in_2__data        =   'tst';                      %default mode
     aRVQx.in_3__maxQ        =   maxQ;                       %max stages
     aRVQx.in_4__M___        =   M;                          %number of codevectors per stage
-    aRVQx.in_5__tSNR        =   tSNR;                %targeted SNR for codebook generation
+    aRVQx.in_5__tSNR        =   tSNR;                       %targeted SNR for codebook generation
     aRVQx.in_6__sw__        =   PARAM.tgt_sw;               %snippet width
     aRVQx.in_7__sh__        =   PARAM.tgt_sh;               %snippet height
-    
+    aRVQx.in_8__type        =   dataType;                   %RVQ data type, 'uint8', 'double'
     aRVQx.in_9__trgD        =   'maxQ';                     %decode stop rule for training vectors
     aRVQx.in_10_tstD        =   tstD;                       %decode stop rule for test vectors
     aRVQx.in_11_lmbd        =   lmbd;                       %lambda, acts like a lagrange multiplier
