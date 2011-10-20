314e38b6fc00a764231f5955dafb27fe418f3438
Salman Aslam <salman.m.aslam@gmail.com>
2011-10-20 10:44:30 -0500

    more writeup for appearance modeling.

--- UTIL_cross_validation_wrapper.m
+++ UTIL_cross_validation_wrapper.m
@@ -7,7 +7,7 @@
 %     close all;
 %     format compact;
     
-function demo_cross_validation(ds_code)
+function UTIL_cross_validation_wrapper(ds_code)
 %algorithm parameters    
     %BPCA
     aBPCA.in_1__name        =   'aBPCA';
@@ -52,25 +52,27 @@ PARAM.tgt_sh                =   sh;
 %-----------------------------
 % 2. PROCESSING
 %-----------------------------
-    %lst_Q                  =   [1:8];
-    %lst_Q                  =   [4:4:256];
-    lst_M                   =   2:1:16;
+    %lst_Q                  =   [1:8];      %TSVQ
+    lst_Q                  =   [4:4:256];  %PCA
+    %lst_M                   =   2:1:16;     %RVQ
     qidx                    =   0;
 
-    %for  q = lst_Q
-    for m=lst_M 
+    for  q = lst_Q
+    %for m=lst_M 
         qidx                =   qidx+1
-        %aBPCA.mdl_1_Q___1x1=   q;
+        aBPCA.mdl_1_Q___1x1=   q;
         %aTSVQ.in_3__maxQ   =   q;
-        aRVQx.in_4__M___    =   m;
-        [aRVQx temp]        =   RVQx_config(PARAM, [], aRVQx.in_3__maxQ, m, aRVQx.in_5__tSNR, rvq__tstI, 0, aRVQx.in_8__type); %0 is lambda
+        %aRVQx.in_4__M___    =   m;
+        %[aRVQx temp]        =   RVQx_config(PARAM, [], aRVQx.in_3__maxQ, m, aRVQx.in_5__tSNR, rvq__tstI, 0, aRVQx.in_8__type); %0 is lambda
         [rmse_trg(qidx), rmse_tst(qidx)]  ... 
-                            =   UTIL_DATA_cross_validation(DM2, aRVQx, numTrials, percentage_tst)
-                            %=   UTIL_DATA_crossvalidation(DM2, aTSVQ, numTrials, percentage_tst)
-                            %=   UTIL_DATA_crossvalidation(DM2, aBPCA, numTrials, percentage_tst)
+                            =   UTIL_DATA_cross_validation(DM2, aBPCA,numTrials, percentage_tst)
+                            %=   UTIL_DATA_cross_validation(DM2, aTSVQ, numTrials,percentage_tst)
+                            %=   UTIL_DATA_cross_validation(DM2, aRVQx, numTrials, percentage_tst)
+                            
+
     end    
 
     rmse_trg
     rmse_tst
-    save(['rvq_cross_validation_' num2str(ds_code)])
+    save(['pca_cross_validation_' num2str(ds_code)])
 
