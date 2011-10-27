b2fabd93ea743809edf9cf6002d702cefd6ce16d
Salman Aslam <salman.m.aslam@gmail.com>
2011-10-27 08:29:08 -0500

    more clean up.

--- demo_RVQ_FILES_read_codebook_file.m
+++ demo_RVQ_FILES_read_codebook_file.m
@@ -18,14 +18,14 @@
 %-------------------------------------
     figure;
     [Q, M, sw, sh, mdl_CB_DxMQ, temp1, temp2]  ...   %temp1 and temp2 have green and blue channels (not needed for single channel)
-                            =  RVQ_FILES_read_codebook_file  ('256_codebook.ecbk'); 
+                            =  RVQ_FILES_read_codebook_file  ('F:\salman\phd\1_Dudek__aRVQ_08_04_1000_0_RofE__\170_codebook.dcbk'); 
     DM2_show(mdl_CB_DxMQ, sh, sw, Q, M, 1);
+    UTIL_FILE_save2pdf('1_Dudek__aRVQ_08_04_1000_0_RofE__170_codebook.pdf', gcf, 300);
     
-    
-    figure;
-    [Q, M, sw, sh, mdl_CB_DxMQ, temp1, temp2]  ...   %temp1 and temp2 have green and blue channels (not needed for single channel)
-                            =  RVQ_FILES_read_codebook_file  ('256_codebook.dcbk'); 
-    DM2_show(mdl_CB_DxMQ, sh, sw, Q, M, 1);
+%     figure;
+%     [Q, M, sw, sh, mdl_CB_DxMQ, temp1, temp2]  ...   %temp1 and temp2 have green and blue channels (not needed for single channel)
+%                             =  RVQ_FILES_read_codebook_file  ('256_codebook.dcbk'); 
+%     DM2_show(mdl_CB_DxMQ, sh, sw, Q, M, 1);
     
 %-------------------------------------
 %POST-PROCESSING
