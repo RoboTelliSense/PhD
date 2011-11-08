85bb30078f5b7ce80ebd0dcbeb38232b310cf941
Salman Aslam <salman.m.aslam@gmail.com>
2011-11-08 06:06:55 -0600

    fixed 5 mistakes pointed out by thesis checking at GT:
    1) date on cover should be December 2011
    
    2)acknowledgments would come before the table of contents.
    
    3) preface would be renamed SUMMARY in the GT dissertation format
    
    4) before you upload, please remove blue, green, and red hyperlinks in document.
    
    5) table captions go above the tables. Make sure tables (i.e. table 2) are within the margins

--- demo_RVQ_FILES_read_codebook_files_in_dir.m
+++ demo_RVQ_FILES_read_codebook_files_in_dir.m
@@ -1,5 +1,16 @@
+    clear;
+    clc;
+    close all;
+    
     figure;
+    
+    for f=5:5:500
+        f
     [Q, M, sw, sh, mdl_CB_DxMQ, temp1, temp2]  ...   %temp1 and temp2 have green and blue channels (not needed for single channel)
-                            =  RVQ_FILES_read_codebook_file  ('F:\salman\phd\1_Dudek__aRVQ_08_04_1000_0_RofE__\170_codebook.dcbk'); 
+                            =  RVQ_FILES_read_codebook_file  (['3_sylv___aRVQ_08_02_1000_0_maxQ__\' num2str(f) '_codebook.dcbk']); 
+                            %=  RVQ_FILES_read_codebook_file
+                            %(['2_david__aRVQ_08_08_1000_0_monR__\' num2str(f) '_codebook.dcbk']); 
     DM2_show(mdl_CB_DxMQ, sh, sw, Q, M, 1);
-    UTIL_FILE_save2pdf('1_Dudek__aRVQ_08_04_1000_0_RofE__170_codebook.pdf', gcf, 300);
+    pause(0.5);
+    %UTIL_FILE_save2pdf('1_Dudek__aRVQ_08_04_1000_0_RofE__170_codebook.pdf', gcf, 300);
+    end
